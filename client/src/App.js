0ae741944f86aad035ae39a4b0504dd61352312d
Karen Stewart <kastewart2002@googlemail.com>
2021-10-29 16:43:39 +0100

    accessing api

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import PlanetariumContainer from './containers/PlanatariumContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PlanetariumContainer />
     </div>
   );
 }
---
+++ client/src/containers/PlanatariumContainer.js
@@ -0,0 +1,92 @@
+import React, {useState, useEffect} from "react";
+// import { ReactComponent as Logo } from "clientassests/download.jpeg";
+import planet from '../assests/download.jpeg';
+
+
+const PlanetariumContainer = () => {
+    const [planets, setPlanets] = useState([]);
+    // const [newPlanets, setNewPlanets] = useState([]);
+    // const [allFetches, setAllFetchs] = useState([]);
+
+    const getPlanets = function(){
+        fetch('https://api.le-systeme-solaire.net/rest/bodies')
+        .then(res => res.json())
+        .then(planets => setPlanets(planets));
+    }
+
+    useEffect(() => {
+        getPlanets();
+        
+    }, [])
+
+    
+
+    // const getPlanets = function() {
+    //     const mercuryData = fetch('https://api.le-systeme-solaire.net/rest/bodies/mercury').then((res) => {
+    //         newPlanets = [...planets]
+    //         newPlanets.push(res)
+    //         setPlanets(newPlanets)
+    //     })
+    //     const venusData = fetch('https://api.le-systeme-solaire.net/rest/bodies/venus').then((res) => {
+    //         newPlanets = [...planets]
+    //         newPlanets.push(res)
+    //         setPlanets(newPlanets)
+    //     })
+    //     const earthData = fetch('https://api.le-systeme-solaire.net/rest/bodies/earth').then((res) => {
+    //         newPlanets = [...planets]
+    //         newPlanets.push(res)
+    //         setPlanets(newPlanets)
+    //     })
+    //     const marsData = fetch('https://api.le-systeme-solaire.net/rest/bodies/mars').then((res) => {
+    //         newPlanets = [...planets]
+    //         newPlanets.push(res)
+    //         setPlanets(newPlanets)
+    //     })
+    //     const jupiterData = fetch('https://api.le-systeme-solaire.net/rest/bodies/jupiter').then((res) => {
+    //         newPlanets = [...planets]
+    //         newPlanets.push(res)
+    //         setPlanets(newPlanets)
+    //     })
+    //     const saturnData = fetch('https://api.le-systeme-solaire.net/rest/bodies/saturn').then((res) => {
+    //         newPlanets = [...planets]
+    //         newPlanets.push(res)
+    //         setPlanets(newPlanets)
+    //     })
+    //     const uranusData = fetch('https://api.le-systeme-solaire.net/rest/bodies/uranus').then((res) => {
+    //         newPlanets = [...planets]
+    //         newPlanets.push(res)
+    //         setPlanets(newPlanets)
+    //     })
+    //     const neptuneData = fetch('https://api.le-systeme-solaire.net/rest/bodies/neptune').then((res) => {
+    //         newPlanets = [...planets]
+    //         newPlanets.push(res)
+    //         setPlanets(newPlanets)
+    //     })
+        
+    //     // allFetches = []
+    //     allFetches.push(mercuryData)
+    //     allFetches.push(venusData)
+    //     allFetches.push(earthData)
+    //     allFetches.push(marsData)
+    //     allFetches.push(jupiterData)
+    //     allFetches.push(saturnData)
+    //     allFetches.push(uranusData)
+    //     allFetches.push(neptuneData)
+    //     Promise.all(allFetches).then(res => res.json())
+
+    // }
+    // console.log(planets.englishName);
+
+    return(
+        <div>
+        <a>
+        <img src={planet}/></a>
+        <h1>hello</h1>
+        
+        </div>
+    )
+
+}
+
+
+export default PlanetariumContainer;
