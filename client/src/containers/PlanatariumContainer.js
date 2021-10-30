94029de0199c4fcdf443a38b0712166ddaeff5ca
Karen Stewart <kastewart2002@googlemail.com>
2021-10-30 17:11:37 +0100

    added planets

--- client/src/containers/PlanatariumContainer.js
+++ client/src/containers/PlanatariumContainer.js
@@ -1,15 +1,19 @@
 import './PlanatariumContainer.css';
 import React, {useState, useEffect} from "react";
 // import { ReactComponent as Logo } from "clientassests/download.jpeg";
-import planet from '../assests/download.jpeg';
-import space from '../assests/space.png'
+import mercury from '../assests/Mercury.png';
+import venus from '../assests/Venus.png';
+import earth from '../assests/Earth.png';
+import mars from '../assests/Mars.png';
+import jupiter from '../assests/Jupiter.png';
+import saturn from '../assests/Saturn.png';
+import uranus from '../assests/Uranus.png';
+import neptune from '../assests/Neptune.png';
 
 
 const PlanetariumContainer = () => {
     const [planets, setPlanets] = useState([]);
-    // const [newPlanets, setNewPlanets] = useState([]);
-    // const [allFetches, setAllFetchs] = useState([]);
-   const [onlyPlanets, setOnlyPlanets] = useState([])
+
 
     const getPlanets = function(){
         fetch('https://api.le-systeme-solaire.net/rest/bodies/')
@@ -45,9 +49,17 @@ const PlanetariumContainer = () => {
         
 
         <div>
-        <a>
-        <img src={planet} class="rotate" key ="earth" onHover={() => onClickPlanet("earth")}/></a>
-        <h1>hello</h1>
+        <h1>Our Planatarium</h1>
+            <a><img src={mercury} key="mercury" onClick={() => onClickPlanet("mercury")}/></a>
+            <a><img src={venus} key ="earth" onClick={() => onClickPlanet("venus")}/></a>
+            <a><img src={earth} key ="earth" onClick={() => onClickPlanet("earth")}/></a>
+            <a><img src={mars} key ="earth" onClick={() => onClickPlanet("mars")}/></a>
+            <a><img src={jupiter} key ="earth" onClick={() => onClickPlanet("jupiter")}/></a>
+            <a><img src={saturn} key ="earth" onClick={() => onClickPlanet("saturn")}/></a>
+            <a><img src={uranus} key ="earth" onClick={() => onClickPlanet("uranus")}/></a>
+            <a><img src={neptune} key ="earth" onClick={() => onClickPlanet("neptune")}/></a>
+
+        
         
 
         {/* <PlanetsDetails planets={planets} /> */}
