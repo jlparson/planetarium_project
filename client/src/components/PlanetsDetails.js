8e81158c59f7a979d93aeebe6dc34010d3786321
pcygan44 <p.cygan44@gmail.com>
2021-10-31 15:55:00 +0000

    adding spin jupiter and extradetais for planet

--- client/src/components/PlanetsDetails.js
+++ client/src/components/PlanetsDetails.js
@@ -1,2 +1,23 @@
 import React from "react";
+import "../App.css"
 
+const  PlanetsDetails  = ({planetInfo}) => {
+
+    
+
+    if (!planetInfo){
+         return (null)
+    }
+
+    
+    return(
+        <div className="planetInfo">
+            <h3>Name:{planetInfo.englishName}</h3>
+            <p>Gravity:{planetInfo.gravity} </p>
+            <p>Density:{planetInfo.density} </p>
+       
+             
+        </div>
+    )
+}
+export default PlanetsDetails ;
--- client/src/containers/PlanatariumContainer.js
+++ client/src/containers/PlanatariumContainer.js
@@ -9,11 +9,12 @@ import jupiter from '../assests/Jupiter.png';
 import saturn from '../assests/Saturn.png';
 import uranus from '../assests/Uranus.png';
 import neptune from '../assests/Neptune.png';
-
+import PlanetsDetails  from '../components/PlanetsDetails';
+import '../SpinningPlanets.css'
 
 const PlanetariumContainer = () => {
     const [planets, setPlanets] = useState([]);
-
+    let [planetInfo, setPlanetInfo] = useState(null);
 
     const getPlanets = function(){
         fetch('https://api.le-systeme-solaire.net/rest/bodies/')
@@ -24,12 +25,14 @@ const PlanetariumContainer = () => {
     }
 
     
+    
     const onClickPlanet = function (id){
         fetch(`https://api.le-systeme-solaire.net/rest/bodies/${id}`)
         .then(res => res.json())
-        .then(res => console.log(res))
+        .then(planetInfo => setPlanetInfo(planetInfo))
+        
     }
-     
+
 
 
     useEffect(() => {
@@ -47,24 +50,26 @@ const PlanetariumContainer = () => {
 
     return(
         
-
+    <>
         <div>
         <h1>Our Planatarium</h1>
+        </div>
         <div className="planetContainer">
             <a id="mercury"><img src={mercury} className="mercury" key="mercury" onClick={() => onClickPlanet("mercury")}/></a>
             <a><img src={venus} className="venus" key="venus" onClick={() => onClickPlanet("venus")}/></a>
             <a><img src={earth} className="earth" key="earth" onClick={() => onClickPlanet("earth")}/></a>
             <a><img src={mars} className="mars" key="mars" onClick={() => onClickPlanet("mars")}/></a>
-            <a><img src={jupiter} className="jupiter" key="jupiter" onClick={() => onClickPlanet("jupiter")}/></a>
+            <a className="jupiter" key="jupiter" onClick={() => onClickPlanet("jupiter")}></a>
             <a><img src={saturn} className="saturn" key="saturn" onClick={() => onClickPlanet("saturn")}/></a>
             <a><img src={uranus} className="uranus" key="uranus" onClick={() => onClickPlanet("uranus")}/></a>
             <a><img src={neptune} className="neptune" key="neptune" onClick={() => onClickPlanet("neptune")}/></a>
         </div>
         
-        
-
-        {/* <PlanetsDetails planets={planets} /> */}
+        <div>
+          <PlanetsDetails  planetInfo= { planetInfo}  onClickPlanet = {onClickPlanet} />
         </div>
+    </>    
+
     )
     
 }
