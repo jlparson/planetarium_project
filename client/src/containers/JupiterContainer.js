b068595e681c153ac4965bdb8382834e3a04fa56
pcygan44 <p.cygan44@gmail.com>
2021-11-01 11:02:43 +0000

    3 commit

--- client/src/containers/JupiterContainer.js
+++ client/src/containers/JupiterContainer.js
@@ -1,11 +1,17 @@
 import jupiter from '../assests/Jupiter.png';
+import '../SpinningPlanets.css'
 
 const Jupiter = () => {
 
     return (
         <>
             <h1>This is Jupiter</h1>
-            <img src={jupiter} alt="Jupiter" />
+            <div className="planetContainer">
+       
+            <a className="jupiter" key="jupiter" ></a>
+      
+
+        </div>
         </>
     );
 };
