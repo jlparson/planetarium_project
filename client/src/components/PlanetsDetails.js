eab56864cf6207e66cb6f607c4e127a0f86ba693
Andrew Crossan <andrewcrossanit@gmail.com>
2021-11-01 17:23:52 +0000

    fixed a small error in PlanetDetails.js

--- client/src/components/PlanetsDetails.js
+++ client/src/components/PlanetsDetails.js
@@ -20,7 +20,6 @@ const  PlanetsDetails  = ({planetInfo}) => {
             <p data-tip="An invisible force that pulls objects toward each other. Earth's gravity is what keeps you on the ground and what makes things fall.">Gravity: {planetInfo.gravity}m/s²</p>
             <p data-tip="How compact the Planet is" >Density: {planetInfo.density}g/cm³</p>
             <a href={`/${planetInfo.englishName}`}>Click here for more info about {planetInfo.englishName}</a>
-            <img src={planetInfo.id} alt="Earth" />
             <ReactTooltip />
        
              
