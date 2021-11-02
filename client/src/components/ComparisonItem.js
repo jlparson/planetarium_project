6ba13144036daf87a886bfaded395d72893e5699
Karen Stewart <kastewart2002@googlemail.com>
2021-11-02 11:01:58 +0000

    planets added

--- client/src/components/ComparisonItem.js
+++ client/src/components/ComparisonItem.js
@@ -11,15 +11,11 @@ const ComparisonItem = ({planetInfo}) => {
     
     return (
         <div>
-        <img src={require(`../assests/${planetInfo.englishName}.png`).default} />   
-
-
+            <img src={require(`../assests/${planetInfo.englishName}.png`).default} />   
             <h3>{planetInfo.englishName}</h3>
-            <img src="../assests/Venus.png" />
             <h3>{planetInfo.density}</h3>
             //greatest distance between the planet and the sun */}
             <h3>{planetInfo.aphelion}</h3> 
-            
             
             
         </div>
